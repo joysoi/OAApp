18fe382ed3ca93ca76dd1ffb43eec64db35296ee
Nikola Nikolovski <nikola.joysoi@gmail.com>
2019-10-10 13:53:53 -0400

    initial commit

---
+++ app/src/main/java/com/example/oaapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.oaapp.ui.auth
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.oaapp.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
